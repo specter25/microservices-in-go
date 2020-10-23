cc6da71d9eb1f09e5df2a2f73b2b920b896e7f55
specter25 <ujjwalcoding012@gmail.com>
2020-10-23 20:50:12 +0530

    custom server setup -- episode2

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/specter25/microservices-in-go/handlers"
 )
@@ -17,6 +18,13 @@ func main() {
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe(":9090", sm)
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
 
+	s.ListenAndServe()
 }
