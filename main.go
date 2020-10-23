8d591fdd49717a48a679b8c2be6e0fb90693f8d1
specter25 <ujjwalcoding012@gmail.com>
2020-10-23 21:42:13 +0530

    proper shutdown of server-- episode2

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
 	"github.com/specter25/microservices-in-go/handlers"
@@ -26,5 +28,24 @@ func main() {
 		WriteTimeout: 1 * time.Second,
 	}
 
-	s.ListenAndServe()
+	// go func used so that it does not stop running
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	//now everything gets blocked here untill the message is received
+	sig := <-sigChan
+	l.Println("Received terminate , graceful shutdown", sig)
+	//This is very important to shutdown the server after it has received all the request
+
+	//shutdown needs a context
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
