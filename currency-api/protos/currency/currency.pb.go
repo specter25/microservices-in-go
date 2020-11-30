62f5a4cf73e614a7fd4d52c16dff0652347b278e
specter25 <ujjwalcoding012@gmail.com>
2020-11-30 22:22:20 +0530

    added bidirectional streaming in grpc

--- currency-api/protos/currency/currency.pb.go
+++ currency-api/protos/currency/currency.pb.go
@@ -308,11 +308,14 @@ var file_currency_proto_rawDesc = []byte{
 	0x4d, 0x58, 0x4e, 0x10, 0x1a, 0x12, 0x07, 0x0a, 0x03, 0x4d, 0x59, 0x52, 0x10, 0x1b, 0x12, 0x07,
 	0x0a, 0x03, 0x4e, 0x5a, 0x44, 0x10, 0x1c, 0x12, 0x07, 0x0a, 0x03, 0x50, 0x48, 0x50, 0x10, 0x1d,
 	0x12, 0x07, 0x0a, 0x03, 0x53, 0x47, 0x44, 0x10, 0x1e, 0x12, 0x07, 0x0a, 0x03, 0x54, 0x48, 0x42,
-	0x10, 0x1f, 0x12, 0x07, 0x0a, 0x03, 0x5a, 0x41, 0x52, 0x10, 0x20, 0x32, 0x32, 0x0a, 0x08, 0x43,
+	0x10, 0x1f, 0x12, 0x07, 0x0a, 0x03, 0x5a, 0x41, 0x52, 0x10, 0x20, 0x32, 0x65, 0x0a, 0x08, 0x43,
 	0x75, 0x72, 0x72, 0x65, 0x6e, 0x63, 0x79, 0x12, 0x26, 0x0a, 0x07, 0x47, 0x65, 0x74, 0x52, 0x61,
 	0x74, 0x65, 0x12, 0x0c, 0x2e, 0x52, 0x61, 0x74, 0x65, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74,
-	0x1a, 0x0d, 0x2e, 0x52, 0x61, 0x74, 0x65, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x62,
-	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x1a, 0x0d, 0x2e, 0x52, 0x61, 0x74, 0x65, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12,
+	0x31, 0x0a, 0x0e, 0x53, 0x75, 0x62, 0x73, 0x63, 0x72, 0x69, 0x62, 0x65, 0x52, 0x61, 0x74, 0x65,
+	0x73, 0x12, 0x0c, 0x2e, 0x52, 0x61, 0x74, 0x65, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a,
+	0x0d, 0x2e, 0x52, 0x61, 0x74, 0x65, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x28, 0x01,
+	0x30, 0x01, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -338,9 +341,11 @@ var file_currency_proto_depIdxs = []int32{
 	0, // 0: RateRequest.Base:type_name -> Currencies
 	0, // 1: RateRequest.Destination:type_name -> Currencies
 	1, // 2: Currency.GetRate:input_type -> RateRequest
-	2, // 3: Currency.GetRate:output_type -> RateResponse
-	3, // [3:4] is the sub-list for method output_type
-	2, // [2:3] is the sub-list for method input_type
+	1, // 3: Currency.SubscribeRates:input_type -> RateRequest
+	2, // 4: Currency.GetRate:output_type -> RateResponse
+	2, // 5: Currency.SubscribeRates:output_type -> RateResponse
+	4, // [4:6] is the sub-list for method output_type
+	2, // [2:4] is the sub-list for method input_type
 	2, // [2:2] is the sub-list for extension type_name
 	2, // [2:2] is the sub-list for extension extendee
 	0, // [0:2] is the sub-list for field type_name
@@ -412,6 +417,7 @@ const _ = grpc.SupportPackageIsVersion6
 type CurrencyClient interface {
 	// GetRate returns the exchange rate for the two provided currency codes
 	GetRate(ctx context.Context, in *RateRequest, opts ...grpc.CallOption) (*RateResponse, error)
+	SubscribeRates(ctx context.Context, opts ...grpc.CallOption) (Currency_SubscribeRatesClient, error)
 }
 
 type currencyClient struct {
@@ -431,10 +437,42 @@ func (c *currencyClient) GetRate(ctx context.Context, in *RateRequest, opts ...g
 	return out, nil
 }
 
+func (c *currencyClient) SubscribeRates(ctx context.Context, opts ...grpc.CallOption) (Currency_SubscribeRatesClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_Currency_serviceDesc.Streams[0], "/Currency/SubscribeRates", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &currencySubscribeRatesClient{stream}
+	return x, nil
+}
+
+type Currency_SubscribeRatesClient interface {
+	Send(*RateRequest) error
+	Recv() (*RateResponse, error)
+	grpc.ClientStream
+}
+
+type currencySubscribeRatesClient struct {
+	grpc.ClientStream
+}
+
+func (x *currencySubscribeRatesClient) Send(m *RateRequest) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *currencySubscribeRatesClient) Recv() (*RateResponse, error) {
+	m := new(RateResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // CurrencyServer is the server API for Currency service.
 type CurrencyServer interface {
 	// GetRate returns the exchange rate for the two provided currency codes
 	GetRate(context.Context, *RateRequest) (*RateResponse, error)
+	SubscribeRates(Currency_SubscribeRatesServer) error
 }
 
 // UnimplementedCurrencyServer can be embedded to have forward compatible implementations.
@@ -443,6 +481,9 @@ type UnimplementedCurrencyServer struct {
 
 func (*UnimplementedCurrencyServer) GetRate(context.Context, *RateRequest) (*RateResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetRate not implemented")
+}
+func (*UnimplementedCurrencyServer) SubscribeRates(Currency_SubscribeRatesServer) error {
+	return status.Errorf(codes.Unimplemented, "method SubscribeRates not implemented")
 }
 
 func RegisterCurrencyServer(s *grpc.Server, srv CurrencyServer) {
@@ -467,6 +508,32 @@ func _Currency_GetRate_Handler(srv interface{}, ctx context.Context, dec func(in
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Currency_SubscribeRates_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(CurrencyServer).SubscribeRates(&currencySubscribeRatesServer{stream})
+}
+
+type Currency_SubscribeRatesServer interface {
+	Send(*RateResponse) error
+	Recv() (*RateRequest, error)
+	grpc.ServerStream
+}
+
+type currencySubscribeRatesServer struct {
+	grpc.ServerStream
+}
+
+func (x *currencySubscribeRatesServer) Send(m *RateResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *currencySubscribeRatesServer) Recv() (*RateRequest, error) {
+	m := new(RateRequest)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 var _Currency_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "Currency",
 	HandlerType: (*CurrencyServer)(nil),
@@ -476,6 +543,13 @@ var _Currency_serviceDesc = grpc.ServiceDesc{
 			Handler:    _Currency_GetRate_Handler,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "SubscribeRates",
+			Handler:       _Currency_SubscribeRates_Handler,
+			ServerStreams: true,
+			ClientStreams: true,
+		},
+	},
 	Metadata: "currency.proto",
 }
--- currency-api/server/currency.go
+++ currency-api/server/currency.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"context"
+	"io"
+	"time"
 
 	"github.com/hashicorp/go-hclog"
 	data "github.com/specter25/microservices-in-go/currency-api/data"
@@ -25,4 +27,31 @@ func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos
 		return nil, err
 	}
 	return &protos.RateResponse{Rate: rate}, nil
+}
+
+func (c *Currency) SubscribeRates(src protos.Currency_SubscribeRatesServer) error {
+
+	go func() {
+		for {
+			rr, err := src.Recv()
+			if err != io.EOF {
+				c.log.Error("Client has closed connections")
+				break
+			}
+			if err != nil {
+				c.log.Error("unable to read form client ", "error", err)
+				break
+			}
+			c.log.Info("handle client request", "request", rr)
+		}
+	}()
+
+	// to handle the message send functionality
+	for {
+		err := src.Send(&protos.RateResponse{Rate: 12.1})
+		if err != nil {
+			return err
+		}
+		time.Sleep(5 * time.Second)
+	}
 }
